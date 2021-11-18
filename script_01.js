d0ea743e569fd3b824b0c89f70f9fd5a72a22c7d
Kakarott <sonntag_thomas_86@web.de>
2021-11-18 11:09:04 +0100

    Variante 1-3 hinzugefügt

--- script_01.js
+++ script_01.js
@@ -1,2 +1,29 @@
 /***** Variablen 01 *******/
 
+/**
+ * 
+ * Block Kommentar
+ */
+
+// Zeilen Kommentar
+
+//  Konsolen ausgabe
+// console.log("Hey Ho!"); // eigentlich Hello world
+// console.log(firstName); // nicht deklarierte variable ---> Fehler
+
+/****** 02 Deklaration + Wertzuweisung ********/
+
+// let firstName;  // Deklaration (Definition)
+// firstName = "Max";  // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log(familyName);    // Ausgabe
+// console.log(firstName + " " + familyName);
+
+/****** 03 Deklaration + Wertzuweisung II ********/
+
+let firstName, familyName;
+firstName = prompt("Bitte Vornamen eingeben")
+familyName = prompt("Bitte Nachnamen eingeben");
+console.log(firstName + " " + familyName);
