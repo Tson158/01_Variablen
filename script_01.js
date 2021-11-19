e8a1f593b2f8038398dee363b063b4f432322073
Kakarott <sonntag_thomas_86@web.de>
2021-11-19 09:14:52 +0100

    Hausaufgaben++

--- script_01.js
+++ script_01.js
@@ -45,24 +45,86 @@
 
 // Deklaration
 
-let ageJohn, ageMark;
-let birthYearJohn, birthYearMark;
+// let ageJohn, ageMark;
+// let birthYearJohn, birthYearMark;
 
-let date = new Date();   //TimeStamp (Zeitstempel)
-let year = date.getFullYear();
+// let date = new Date();   //TimeStamp (Zeitstempel)
+// let year = date.getFullYear();
 
 // Wertzuweisung
 
-ageJohn = 25;
-ageMark = 30;
+// ageJohn = 25;
+// ageMark = 30;
 
 //  Berechnung
 
 // let year = 2021;
- birthYearJohn = year - ageJohn;
- birthYearMark = year - ageMark;
+//  birthYearJohn = year - ageJohn;
+//  birthYearMark = year - ageMark;
 
 //  Ausgabe
 
-console.log("birthYearJohn" + birthYearJohn);
-console.log("birthYearMark" + birthYearMark);
+// console.log("birthYearJohn" + birthYearJohn);
+// console.log("birthYearMark" + birthYearMark);
+
+
+// Hausaufgabe *** 18.11.2021 ***
+
+// let zahl1;
+// let zahl2;  
+// let ergebnis;
+
+// zahl1 = prompt("Zahl einfügen : ");
+// zahl2 = prompt("Zahl einfügen : ");
+// ergebnis = (parseInt(zahl1) + parseInt(zahl2));
+
+// console.log("Das Ergebnis ist: " + ergebnis);
+
+// Hausaufgabe lösung Variante 1 *******
+// Vorteil : gute Lesbarkeit | aber viele Variablen
+
+// Deklaration
+
+// let ziffer1, ziffer2;
+// let zahl1, zahl2, summe;
+
+// // Eingabe
+
+// ziffer1 = prompt("Bitte Zahl1 einfügen");
+// ziffer2 = prompt("Bitte Zahl2 einfügen");
+// console.log(typeof ziffer1);
+// console.log(typeof ziffer2);
+
+// // Typkonvertierung | ziffer ---> zahl (string --> number)
+
+// zahl1 = parseInt(ziffer1);
+// zahl2 = parseInt(ziffer2);
+// console.log(typeof zahl1);
+// console.log(typeof zahl2);
+
+// // Berechnung
+
+// summe = zahl1 + zahl2;
+//  console.log("Die summe ist: " + summe);
+
+// // Variante 2 *****************
+// //  Kompakte Lösung
+
+// let zahl1, zahl2, summe;
+
+// zahl1 = parseInt(prompt("Bitte Zahl 1 eingeben"));
+// zahl2 = parseInt(prompt("Bitte Zahl 2 eingeben"));
+// summe = zahl1 + zahl2;
+// console.log("Die summe ist: " + summe);
+
+
+// // Variante 3 *******************
+
+ console.log
+ (
+     "die Summe ist: " +
+   (
+       parseFloat(prompt("Bitte Zahl 1 eingeben")) +
+      parseFloat(prompt("Bitte Zahl 2 eingeben"))
+   )
+ );
